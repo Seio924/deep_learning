3ffdc546be1d6fa31795a957d4d3af73226c0696
Seio924 <134473627+Seio924@users.noreply.github.com>
2024-01-27 19:18:35 +0900

    Project2

--- Project2/fashion.py
+++ Project2/fashion.py
@@ -5,8 +5,18 @@ import matplotlib.pyplot as plt
 
 #사진 보여주기
 #plt.imshow(trainX[0])
+#plt.gray()
+#plt.colorbar()
+#plt.show()
+
+trainX = trainX / 255.0
+testX = testX / 255.0
 
 
+trainX.reshape((trainX.shape[0], 28, 28, 1))
+testX.reshape((testX.shape[0], 28, 28, 1))
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 
 #확률 loss 함수 : cross entropy
@@ -16,7 +26,16 @@ import matplotlib.pyplot as plt
 #softmax : 결과를 0~1로 압축 -> 카테고리 예측문제에 사용 [예측한 10개의 확률을 다 더하면 1 나옴]
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu", input_shape="(28, 28, 1)"),
+    tf.keras.layers.MaxPoolong2D((2, 2)),
+    #tf.keras.layers.Dense(128, input_shape=(28,28), activation="relu"),
+    tf.keras.layers.Flatten(), #일렬로 나열
     tf.keras.layers.Dense(64, activation="relu"),
     tf.keras.layers.Dense(10, activation="softmax"),
 ])
+
+model.summary()
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(trainX, trainY, epochs=5)
